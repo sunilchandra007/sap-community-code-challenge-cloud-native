9e6577d4937c2bc7e82a686ff02636c7c7fe4440
Sunil Chandra <sunilchandra007@gmail.com>
2022-07-19 14:09:31 +0800

    Update server.js

--- service/server.js
+++ service/server.js
@@ -9,6 +9,10 @@ app.get('/greeting/:communityID', (req, res) => {
   res.send(`Hey ${req.params.communityID}, really nice seeing you here! 🙂`);
 });
 
+app.get('/kyma', (req, res) => {
+  res.send(`Hello Kyma 🙂`);
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
